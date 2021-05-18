215643623110b59ec2500a29bde78338dd0c577c
rohitb <rbhogal23@gmail.com>
2021-05-17 17:40:41 -0700

    unique user id fetch

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class UserHeader extends React.Component {
-    render() {
-        return <div></div>
+  render() {
+    const { user } = this.props;
+    if (!user) {
+      return null;
     }
+
+    return <div className="header">{user.name}</div>;
+  }
 }
 
-export default UserHeader;
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
+};
+
+export default connect(mapStateToProps)(UserHeader);
